9739e3ebcfc84dc39d9ea65d7df85a618b9fc133
MiguelPelegrina <95084406+MiguelPelegrina@users.noreply.github.com>
2024-03-22 09:36:45 +0100

    Updated code for fake products

--- tests/factories.py
+++ tests/factories.py
@@ -31,4 +31,9 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    ## Add code to create Fake Products 
+    name = factory.Faker("name")
+    description = factory.Faker("lorem")
+    price = FuzzyDecimal()
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(choices=[Category])
